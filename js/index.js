94694a69c919376bf6c4372e20d4fc6498ca3613
vinoth <vinoth.vinothrenga@gmail.com>
2017-05-01 02:13:10 +0530

    getting index value

--- js/index.js
+++ js/index.js
@@ -2,13 +2,33 @@ var app = angular.module('app', ['ngPowerPoint']);
 
 var ngPowerPoint = angular.module('ngPowerPoint', []);
 ngPowerPoint.directive('ngPowerPoint', function() {
+	
 	return {
-		template: '<textarea rows="4" cols="50"></textarea>',
-		restrict: 'E'
+		templateUrl: '../js/textarea.html',
+		restrict: 'E',
+		scope: {},
+		transclude: true,
+		controller: ['$scope', function($scope) {
+
+		}]
+	}
+});
+ngPowerPoint.directive('pptIndex', function() {
+	function link(scope, element, attrs, timerctrl) {
+		console.log(scope.pptIndex);	
+	}
+	return {
+		require: '^^ngPowerPoint',
+		restrict: 'A',
+		transclude: true,
+		scope: {
+			pptIndex: '@'
+		},
+		link: link
 	}
 });
 
 app.controller('ngCtrl', ['$scope', function($scope) {
-	$scope.myname = "vinoth";
+
 }]);
 
